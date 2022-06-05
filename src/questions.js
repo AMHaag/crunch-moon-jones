89450ee3d55112c290744dc9d4f4384dbf06220c
Aaron Haag <101981364+AMHaag@users.noreply.github.com>
2022-06-04 23:16:10 -0400

    basic questions complete

--- src/questions.js
+++ src/questions.js
@@ -57,45 +57,99 @@ const engineerPrompts = [
   },
 ];
 
+const internPrompts = [
+  {
+    prefix: "Intern",
+    name: "int_name",
+    type: "input",
+    message: "What is their name?",
+  },
+  {
+    prefix: "Intern",
+    name: "int_id",
+    type: "input",
+    message: "What is their employee ID?",
+  },
+  {
+    prefix: "Intern",
+    name: "int_email",
+    type: "input",
+    message: "What is their email address?",
+  },
+  {
+    prefix: "Intern",
+    name: "int_school",
+    type: "input",
+    message: "What school did they attend?",
+  },
+];
+
+const forkPrompt = {
+  type: "list",
+  name: "fork",
+  message: "What would you like to do?",
+  choices: [
+    "Add an engineer to my team",
+    "Add an intern to my team",
+    "Finish building team",
+  ],
+  default:""
+  filter(val) {
+    let valParse = val.split(" ");
+    if (valParse[2] === "engineer") {
+      return "engineer";
+    }
+    if (valParse[2] === "intern") {
+      return "intern";
+    } else {
+      return "finish";
+    }
+  },
+};
+
 //* module variables to store answers formatted as objects
 const teamLeader = {};
 const engineerArray = [];
 const internArray = [];
 
+//*these three functions asks questions and write their answers to global objects/arrays
 const askTeamLeader = function () {
-  inquirer
-    .prompt(managerPrompts)
-    .then((a) => {
-        // const x = new Manager(a.mng_name,a.mng_id,a.mng_email,a.mng_office);
-        // let teamleader = x;
-      teamLeader.name = a.mng_name;
-      teamLeader.id = a.mng_id;
-      teamLeader.email = a.mng_email;
-      teamLeader.officeNumber = a.mng_office
-    })}
-
-
+  inquirer.prompt(managerPrompts).then((a) => {
+    teamLeader.name = a.mng_name;
+    teamLeader.id = a.mng_id;
+    teamLeader.email = a.mng_email;
+    teamLeader.officeNumber = a.mng_office;
+  });
+};
 const addEngineer = function () {
   inquirer.prompt(engineerPrompts).then((a) => {
-    const eng = new Engineer(a.eng_name,a.eng_id,a.eng_email,a.eng_GitHub);
+    const eng = new Engineer(a.eng_name, a.eng_id, a.eng_email, a.eng_GitHub);
     engineerArray.push(eng);
   });
 };
-
-const addIntern = function(){
-    inquirer.prompt()
-}
-
+const addIntern = function () {
+  inquirer.prompt(internPrompts).then((a) => {
+    const int = new Intern(a.int_name, a.int_id, a.int_email, a.int_school);
+    internArray.push(int);
+  });
+};
+const fork = function () {
+  inquirer.prompt(forkPrompt).then((a) => {
+    console.log(a);
+  });
+};
 // askTeamLeader();
-newEngineer()
+// addEngineer()
+// addIntern()
+fork();
 
 let x = () => {
   console.log(teamLeader);
   console.log(engineerArray);
   console.log(internArray);
 };
-setTimeout(newEngineer,5000)
-setTimeout(x, 10000);
+// setTimeout(newEngineer,5000)
+// setTimeout(x, 10000);
 module.exports = askTeamLeader;
 module.exports = engineerArray;
 module.exports = internArray;
